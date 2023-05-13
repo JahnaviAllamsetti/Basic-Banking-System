4090b00aeda7c28eb28ac89355379d71359d904e
Jahnavi <105658847+JahnaviAllamsetti@users.noreply.github.com>
2023-05-13 10:51:30 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -19,12 +19,12 @@
     </nav>
     <!-- navbar ends here  -->
 
-    <div class="container" style="display:flex;justify-content: space-between;">
+    <div class="container">
         <div class="content">
-            <h1 style="font-size:45px;padding:10px;font-weight:bold;">Welcome to Nimbuspay Bank</h1>
-                <p style="font-size:24px;padding:10px;font-weight:lighter;">Your Transactions Made Easy</p>
-                <br>
-                <a class="transfer" href="customers.php">Customers</a>
+            <h1>Welcome to Nimbuspay Bank</h1>
+            <p>Your Transactions Made Easy</p>
+            <br>
+            <a class="transfer" href="customers.php">Customers</a>
         </div>
 
         <div class="image">
